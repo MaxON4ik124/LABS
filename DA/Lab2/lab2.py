2e901df366ad8680cae796d588026b513d9fed64
MaxON4ik124 <maximize326@gmail.com>
2026-03-22 18:38:05 +0300

    DA try

--- DA/Lab2/lab2.py
+++ DA/Lab2/lab2.py
@@ -1,4 +1,13 @@
 import requests
+from bs4 import BeautifulSoup
 
-main_response = requests.get("https://ruz.spbstu.ru")
-print(main_response.text)
+teacher_id = 28961
+date = "2026-3-16"
+url = f"https://ruz.spbstu.ru/teachers/{teacher_id}?date={date}"
+
+r = requests.get(url, headers={"User-Agent": "Mozilla/5.0"}, timeout=20)
+r.raise_for_status()
+r.encoding = "utf-8"
+
+soup = BeautifulSoup(r.text, "html.parser")
+print(soup.get_text("\n", strip=True))
