9797a2e889a316bb916d3b4ec506b65fc66d935f
MaxON4ik124 <maximize326@gmail.com>
2026-04-03 00:28:34 +0300

    Multiclient

--- OS/Lab2/Polygon/cli_run.py
+++ OS/Lab2/Polygon/cli_run.py
@@ -1,14 +1,13 @@
-import cilfilegen
+import cligen
 import os
 def main():
     clients, msgs = map(int, input("Enter count of clients and count of messages:").split())
     for i in range(1, clients+1):
         for g in range(1, msgs+1):
-            cilfilegen.write_to_file(f"file_{i}.txt", f"{i}___msg{g}")
+            cligen.write_to_file(f"file_{i}.txt", f"{i}___msg{g}")
         if i == msgs:
-            cilfilegen.write_to_file(f"file{clients+2}", "stop")
-        os.system(f"./udpclient 192.168.56.1:9005 file{i}.txt")
+            cligen.write_to_file(f"file{clients+2}.txt", "stop")
+        os.system(f"./udpclient 192.168.56.1:9005 file_{i}.txt")
     os.system(f"./udpclient 192.168.56.1:9005 file{clients+2}.txt")
 
-if __name__ == "__main__":
-    main()
+main()
--- OS/Lab2/Polygon/cligen.py
+++ OS/Lab2/Polygon/cligen.py
@@ -7,7 +7,6 @@ def write_to_file(file: str, message: str):
         bbb = random.randint(-2147483648, 2147483648)
         curtime = time.ctime().split()[-2]
         f.write(f"{aa} {bbb} {curtime} {message}\n")
-    f.close()
 
 
 # message_list = []
