7d5fecf00bd2f0492eab54feeadcd0224c918fd9
MaxON4ik124 <maximize326@gmail.com>
2026-04-08 15:47:04 +0300

    Runner pre run

--- OS/Lab2/Polygon/run.rb
+++ OS/Lab2/Polygon/run.rb
@@ -1,14 +1,21 @@
 require './base.rb'
-# timelim(3)
-# clients(1)
+# timelim(4)
+# clients(3)
 
 testing do 
-  srv = server(UDPSRVEMUL, "9070", "9070", "--skipfirst=10")
+  srv = server(TCPSRVEMUL, "9011")
 
-  cli = client(UDPCLIBIN, "127.0.0.1:9070", "cli1.txt")
-  log "Client waiting..."
-  cli.wait
+  cli1 = client(TCPCLIBIN, "127.0.0.1:9011", "cli1.txt")
+  cli2 = client(TCPCLIBIN, "10.0.176.107:9011", "cli2.txt")
 
+  log "Clients 1,2 waiting..."
+  cli1.wait
+  cli2.wait
+
+  cli3 = client(TCPCLIBIN, "127.0.0.1:9011", "cli3.txt")
+  log "Client 3 waiting..."
+  cli3.wait
+  
   log "Server waiting..."
   srv.wait
   log "Server exit code: #{srv.exit_code}"
