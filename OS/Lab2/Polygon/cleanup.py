8707715f060bf3317cf0c3109466875c31b2cd5c
MaxON4ik124 <maximize326@gmail.com>
2026-04-14 14:19:51 +0300

    OS2 maybe ready

--- OS/Lab2/Polygon/cleanup.py
+++ OS/Lab2/Polygon/cleanup.py
@@ -3,8 +3,6 @@ import os
 for file in os.listdir():
     if file.split('.')[-1] == "stdout":
         os.remove(file)
-    if 'msg.txt' in file:
-        os.remove("msg.txt")
     if "cli" and ".txt" in file:
         os.remove(file)
     if "run.log" in file:
