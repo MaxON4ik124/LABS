93d2c73710771f3215b0fdd30ea6b80f08041d59
MaxON4ik124 <maximize326@gmail.com>
2026-08-05 17:02:44 +0500

    New Sem prepare

---
+++ 4Sem/PR/installer.bat
@@ -0,0 +1,5 @@
+winget install -e --id Docker.DockerDesktop
+winget install Kubernetes.Kubectl
+winget install Kubernetes.Minikube
+
+docker context use desktop-linux
--- PR/installer.bat
+++
@@ -1,5 +0,0 @@
-winget install -e --id Docker.DockerDesktop
-winget install -e --id Kubernetes.Kubectl
-winget install -e --id Kubernetes.Minikube
-
-docker context use desktop-linux
